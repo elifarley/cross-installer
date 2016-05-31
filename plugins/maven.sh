c05f70c7488ebc0126a30364a4038daddb173a86
Elifarley C <elifarley@users.noreply.github.com>
2016-05-31 17:46:08 -0300

    Move Maven 3 installation to a plugin (#2)

---
+++ plugins/maven.sh
@@ -0,0 +1,16 @@
+maven_install_maven3() {
+  test $# -ge 2 || {
+    echo "Usage: $0 install maven3 <version> <sha1> [<prefix>=/usr/local]"
+    return 1
+  }
+
+  local version="$1"; shift
+  local sha="$1"; shift
+  local prefix="${1:-/usr/local}"; shift
+
+  curl -fsSL http://www-us.apache.org/dist/maven/maven-3/"$version"/binaries/apache-maven-"$version"-bin.tar.gz \
+    -o /tmp/maven.tgz && echo "$sha  /tmp/maven.tgz" | sha1sum -wc - && \
+    tar -xzf /tmp/maven.tgz -C "$prefix" && rm /tmp/maven.tgz || return $?
+  ln -s apache-maven-"$version" "$prefix"/maven-3 && \
+  ln -s ../maven-3/bin/mvn ../maven-3/bin/mvnDebug ../maven-3/bin/mvnyjp "$prefix"/bin
+}
