d9324683a39e3b8c1d13d60a0575b89dc4395f53
Elifarley C <elifarley@users.noreply.github.com>
2016-05-31 11:52:15 -0300

    Support installing without curl
    via a previously downloaded archive

--- install.sh
+++ install.sh
@@ -2,8 +2,15 @@ prefix="${1:-/usr/local}"; shift
 set -x
 
 mkdir "$prefix"/cross-installer && tmp="$(mktemp -d)" || return $?
-curl -fsSL https://github.com/elifarley/cross-installer/archive/master.tar.gz \
+
+which curl && {
+  curl -fsSL https://github.com/elifarley/cross-installer/archive/master.tar.gz \
   | tar -xz -C "$tmp" || { rm -rf "$tmp"; exit 1 ;}
+
+} || {
+  tar -xzf /tmp/cross-installer.tgz -C "$tmp" || { rm -rf "$tmp"; exit 1 ;}
+}
+
 mv "$tmp"/*/* "$prefix"/cross-installer || { rm -rf "$tmp"; exit 1 ;}
 rm -rf "$tmp" || exit $?
 
