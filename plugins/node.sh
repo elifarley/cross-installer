593fe0e5a29a6b7e95c3f654571445b3336af4f6
Elifarley C <elifarley@users.noreply.github.com>
2016-06-01 16:51:15 -0300

    Create node.sh

---
+++ plugins/node.sh
@@ -0,0 +1,32 @@
+node_install_node() {
+  hascmd apt-get && { node_install_node_debian "$@"; return ;}
+  hascmd apk && { node_install_node_alpine "$@"; return ;}
+  os_version && return 1
+}
+
+node_install_node_debian() {
+  local version="$1"
+
+# gpg keys listed at https://github.com/nodejs/node
+for key in \
+  9554F04D7259F04124DE6B476D5A82AC7E37093B \
+  94AE36675C464D64BAFA68DD7434390BDBE9B9C5 \
+  0034A06D9D9B0064CE8ADF6BF1747F4AD2306D93 \
+  FD3A5288F042B6850C66B31F09FE44734EB7990E \
+  71DCFD284A79C3B38668286BC97EC7A07EDE3FC1 \
+  DD8F2338BAE7501E3DD5AC78C273792F7D83545D \
+  B9AE9905FFD7803F25714661B63B535A4C206CA9 \
+  C4F0DFFF4E8C1A8236409D08E73BC641CC11F4C8 \
+  ; do
+    gpg --keyserver ha.pool.sks-keyservers.net --recv-keys "$key"
+  done
+
+  curl -SLO "https://nodejs.org/dist/v$version/node-v$version-linux-x64.tar.xz" && \
+  curl -SLO "https://nodejs.org/dist/v$version/SHASUMS256.txt.asc" && \
+  gpg --batch --decrypt --output SHASUMS256.txt SHASUMS256.txt.asc && \
+  grep " node-v$version-linux-x64.tar.xz\$" SHASUMS256.txt | sha256sum -c - && \
+  tar -xJf "node-v$version-linux-x64.tar.xz" -C /usr/local --strip-components=1 && \
+  rm "node-v$version-linux-x64.tar.xz" SHASUMS256.txt.asc SHASUMS256.txt || return
+
+  main cleanup
+}
