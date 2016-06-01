de4c90628d304096905c18fc4c42667a9f632900
Elifarley C <elifarley@users.noreply.github.com>
2016-06-01 10:21:43 -0300

    Local archive takes precedence over download

--- install.sh
+++ install.sh
@@ -1,15 +1,19 @@
-prefix="${1:-/usr/local}"; shift
+CMD_BASE="$(readlink -f $0)" || CMD_BASE="$0"; CMD_BASE="$(dirname $CMD_BASE)"
+
 set -x
+
+prefix="${1:-/usr/local}"; shift
 
 mkdir "$prefix"/cross-installer && tmp="$(mktemp -d)" || return $?
 
-which curl && {
+local_archive="$CMD_BASE"/cross-installer.tgz
+test -s "$local_archive" && {
+  tar -xzf "$CMD_BASE"/cross-installer.tgz -C "$tmp" || { rm -rf "$tmp"; exit 1 ;}
+
+} || which curl >/dev/null 2>&1 && {
   curl -fsSL https://github.com/elifarley/cross-installer/archive/master.tar.gz \
   | tar -xz -C "$tmp" || { rm -rf "$tmp"; exit 1 ;}
 
-} || {
-  CMD_BASE="$(readlink -f $0)" || CMD_BASE="$0"; CMD_BASE="$(dirname $CMD_BASE)"
-  tar -xzf "$CMD_BASE"/cross-installer.tgz -C "$tmp" || { rm -rf "$tmp"; exit 1 ;}
 }
 
 mv "$tmp"/*/* "$prefix"/cross-installer || { rm -rf "$tmp"; exit 1 ;}
