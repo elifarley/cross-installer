7545cc0dadc000e95a6e32b05bfc5d64994c1c76
Elifarley C <elifarley@users.noreply.github.com>
2016-06-01 14:34:09 -0300

    new function: "typeof"
    Tested in Bash 4.3 and Ash from BusyBox v1.24.1

--- lib/os.sh
+++ lib/os.sh
@@ -4,3 +4,19 @@ os_version() { (
   test -f /etc/debian_version && VERSION="$(cat /etc/debian_version)"
   echo "$PRETTY_NAME [$VERSION]"
 ) }
+
+# Prints only a word to describe the type of the first argument
+# (alias, builtin, function, file)
+typeof() {
+
+  # Bash
+  type --help >/dev/null 2>&1 || { type -t "$1"; return ;}
+
+  # Ash
+  local result="$(type "$1")"
+  echo $result | grep -oq 'not found' && return 1
+  echo $result | grep -oq alias && result=alias
+  result="${result##* }"; test ${result##/*} || result=file
+  echo $result
+
+}
