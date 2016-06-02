b0b022fa3d724655ee3b12cb60ce803a08c18c78
Elifarley C <elifarley@users.noreply.github.com>
2016-06-02 20:52:56 -0300

    Fix "dash: shift: can't shift that many"

--- plugins/maven.sh
+++ plugins/maven.sh
@@ -6,7 +6,7 @@ maven_install_maven3() {
 
   local version="$1"; shift
   local sha="$1"; shift
-  local prefix="${1:-/usr/local}"; shift
+  local prefix="${1:-/usr/local}"; test $# -gt 0 && shift
 
   curl -fsSL http://www-us.apache.org/dist/maven/maven-3/"$version"/binaries/apache-maven-"$version"-bin.tar.gz \
     -o /tmp/maven.tgz && echo "$sha  /tmp/maven.tgz" | sha1sum -wc - && \
