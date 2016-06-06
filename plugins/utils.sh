b992af6a3b54475c293c65d1c40b11a95ba1e2c7
Elifarley C <elifarley@users.noreply.github.com>
2016-06-06 18:38:16 -0300

    a

--- plugins/utils.sh
+++ plugins/utils.sh
@@ -2,3 +2,7 @@ install_tar_debian() {
   curl -fsSL http://ftp.debian.org/debian/pool/main/t/tar/tar_1.29-1_amd64.deb -o /tmp/tar.deb && \
   dpkg -i /tmp/tar.deb
 }
+
+install_tar_alpine() {
+  apk add tar
+}
