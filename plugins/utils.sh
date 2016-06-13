1a24967b4fd8964fd20295807e093c95193b4a57
Elifarley C <elifarley@users.noreply.github.com>
2016-06-13 19:37:01 -0300

    Delete downloaded package after installation

--- plugins/utils.sh
+++ plugins/utils.sh
@@ -1,6 +1,6 @@
 install_tar_debian() {
   curl -fsSL http://ftp.debian.org/debian/pool/main/t/tar/tar_1.29-1_amd64.deb -o /tmp/tar.deb && \
-  dpkg -i /tmp/tar.deb
+  dpkg -i /tmp/tar.deb && rm -f /tmp/tar.deb
 }
 
 install_tar_alpine() {
