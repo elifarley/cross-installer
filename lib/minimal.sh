0537b7b01cba9127900a162fd399fc3dc27acda7
Elifarley C <elifarley@users.noreply.github.com>
2016-06-13 15:54:38 -0300

    Update minimal.sh

--- lib/minimal.sh
+++ lib/minimal.sh
@@ -1,3 +1,25 @@
+import_shell_lib() {
+  local prefix="${1:-/usr/local}"; test $# -gt 0 && shift
+  test "$(ls -A "$prefix"/shell-lib/lib/* 2>/dev/null)" || return 1
+  for f in "$prefix"/shell-lib/lib/*; do
+    # Skip non POSIX compliant scripts
+    test "$(basename "$f")" = 'misc.sh' && continue
+    test "$(basename "$f")" = 'str.sh' && continue
+    . "$f"
+  done
+}
+
+install_shell_lib() {
+  test "$1" = '--help' && {
+    echo "Usage: $0 install shell-lib <version> <sha1> [<prefix>=/usr/local]"
+    return 1
+  }
+  local version="$1"; test $# -gt 0 && shift
+  set -- "${version:-master}" "$@"
+  untar_url 'https://github.com/elifarley/shell-lib/archive/%s.tar.gz' "$@" && \
+  import_shell_lib
+}
+
 check_sha1() {
   local filepath="$1"; local expected="$2"
   test "$expected" || return 0
