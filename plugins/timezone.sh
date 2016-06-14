6d92a483c286ab4dc0bed71aa31321fdfb7ff411
Elifarley C <elifarley@users.noreply.github.com>
2016-06-14 17:17:04 -0300

    Create timezone.sh

---
+++ plugins/timezone.sh
@@ -0,0 +1,16 @@
+install_timezone_alpine() {
+  test "$TZ" || { echo "TZ is not set"; return 1 ;}
+  apk add --no-cache tzdata || return
+  echo "TZ set to '$TZ'"
+  echo $TZ > /etc/TZ
+  cp -a /usr/share/zoneinfo/"$TZ" /etc/localtime && \
+  apk del tzdata
+}
+
+install_timezone_debian() {
+  test "$TZ" || { echo "TZ is not set"; return 1 ;}
+  # locale-gen $LANG && dpkg-reconfigure locales && /usr/sbin/update-locale LANG=$LANG
+  echo "TZ set to '$TZ'"
+  echo $TZ > /etc/TZ
+  rm -f /etc/localtime && ln -s /usr/share/zoneinfo/"$TZ" /etc/localtime
+}
