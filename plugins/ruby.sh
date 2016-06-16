b6132ed88bbca914f3fa920af7268b48b8a1e572
Elifarley C <elifarley@users.noreply.github.com>
2016-06-16 18:53:37 -0300

    Create ruby.sh

---
+++ plugins/ruby.sh
@@ -0,0 +1,25 @@
+install_ruby_debian() {
+  RUBY_VERSION="${RUBY_VERSION:-2.3.0}"
+  RUBY_MAJOR="${RUBY_VERSION%.*}"
+
+  APT_PACKAGES="\
+gcc g++ make patch binutils libc6-dev \
+  libjemalloc-dev libffi-dev libssl-dev libyaml-dev zlib1g-dev libgmp-dev libxml2-dev \
+  libxslt1-dev libreadline-dev libsqlite3-dev \
+  libpq-dev unixodbc unixodbc-dev unixodbc-bin ruby-odbc freetds-bin freetds-common freetds-dev postgresql-client \
+  git \
+"
+
+  apt-get update && apt-get -y dist-upgrade && \
+  apt-get install -y --no-install-recommends $APT_PACKAGES && \
+  apt-get remove --purge -y $APT_REMOVE_PACKAGES && apt-get autoremove --purge -y && apt-get clean && \
+  ( curl -fsSL "https://cache.ruby-lang.org/pub/ruby/${RUBY_MAJOR}/ruby-${RUBY_VERSION}.tar.gz" | \
+    tar -xzC /tmp && \
+    cd /tmp/ruby-${RUBY_VERSION} && \
+    ./configure --enable-shared --with-jemalloc --disable-install-doc && \
+    make -j4 && make install && \
+    rm /usr/local/lib/libruby-static.a && \
+    cd /tmp && rm -rf /tmp/* \
+  )
+
+}
