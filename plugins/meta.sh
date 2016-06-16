afd636c8dbd8daaf401692e25649359eea6470c6
Elifarley C <elifarley@users.noreply.github.com>
2016-06-16 10:34:32 -0300

    Remove shell-lib

--- plugins/meta.sh
+++ plugins/meta.sh
@@ -1,5 +1,6 @@
 meta_remove() {
   local prefix="$(readlink -f "$CMD_BASE"/../..)"
+  remove_shell_lib /usr/local/shell-lib
   echo "[meta] Removing cross-installer from prefix '$prefix'..."
   rm -rv "$prefix"/cross-installer* "$prefix"/bin/xinstall && \
   echo "[meta] OK - cross-installer has been removed from prefix '$prefix'."
