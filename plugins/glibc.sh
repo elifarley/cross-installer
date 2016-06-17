ba2a0b6e5fb62e08613b7665b551e658cfecdaaa
Elifarley C <elifarley@users.noreply.github.com>
2016-06-17 10:14:23 -0300

    Use xinstall

--- plugins/glibc.sh
+++ plugins/glibc.sh
@@ -7,7 +7,7 @@ install_glibc_alpine() {
   ALPINE_GLIBC_BASE_PACKAGE_FILENAME="glibc-$ALPINE_GLIBC_PACKAGE_VERSION.apk" && \
   ALPINE_GLIBC_BIN_PACKAGE_FILENAME="glibc-bin-$ALPINE_GLIBC_PACKAGE_VERSION.apk" && \
   ALPINE_GLIBC_I18N_PACKAGE_FILENAME="glibc-i18n-$ALPINE_GLIBC_PACKAGE_VERSION.apk" && \
-  apk add --no-cache --virtual=build-dependencies && \
+  main install-pkg --virtual=build-dependencies && \
   curl -fsSL \
       "https://raw.githubusercontent.com/andyshinn/alpine-pkg-glibc/master/sgerrand.rsa.pub" \
       -o "/etc/apk/keys/sgerrand.rsa.pub" && \
@@ -15,7 +15,7 @@ install_glibc_alpine() {
       "$ALPINE_GLIBC_BASE_URL/$ALPINE_GLIBC_PACKAGE_VERSION/$ALPINE_GLIBC_BASE_PACKAGE_FILENAME" \
       "$ALPINE_GLIBC_BASE_URL/$ALPINE_GLIBC_PACKAGE_VERSION/$ALPINE_GLIBC_BIN_PACKAGE_FILENAME" \
       "$ALPINE_GLIBC_BASE_URL/$ALPINE_GLIBC_PACKAGE_VERSION/$ALPINE_GLIBC_I18N_PACKAGE_FILENAME" && \
-  apk add --no-cache \
+  main install-pkg \
       "$ALPINE_GLIBC_BASE_PACKAGE_FILENAME" \
       "$ALPINE_GLIBC_BIN_PACKAGE_FILENAME" \
       "$ALPINE_GLIBC_I18N_PACKAGE_FILENAME" && \
@@ -24,9 +24,7 @@ install_glibc_alpine() {
   /usr/glibc-compat/bin/localedef --force --inputfile POSIX --charmap "${LANG##*.}" "$LANG" || true && \
   echo "export LANG=$LANG" > /etc/profile.d/locale.sh && \
   \
-  apk del glibc-i18n && \
-  \
-  apk del build-dependencies && \
+  main remove-pkg glibc-i18n build-dependencies && \
   rm \
       "$ALPINE_GLIBC_BASE_PACKAGE_FILENAME" \
       "$ALPINE_GLIBC_BIN_PACKAGE_FILENAME" \
