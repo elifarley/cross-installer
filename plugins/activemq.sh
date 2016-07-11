baf114145888224f1df794ecb83275cd9ca487c4
Elifarley C <elifarley@users.noreply.github.com>
2016-07-11 16:57:58 -0300

    Improve handling of tmpdir and data dir

--- plugins/activemq.sh
+++ plugins/activemq.sh
@@ -11,16 +11,18 @@ add_activemq() {
   local prefix="${1:-/usr/local}"; test $# -gt 0 && shift
   local url="$(printf "$url_activemq" "$version" "$version")"
   untar_url "$url" "$version" "$sha" "$prefix" && \
-  ( cd "$prefix"/apache-activemq && pwd && ls -Falk && \
+  ( cd "$prefix"/apache-activemq && \
+    rm -rfv data && ln -s /data .
     mv conf/activemq.xml conf/activemq.xml.orig && \
     awk '/.*stomp.*/{print "            <transportConnector name=\"stompssl\" uri=\"stomp+nio+ssl://0.0.0.0:61612?transport.enabledCipherSuites=SSL_RSA_WITH_RC4_128_SHA,SSL_DH_anon_WITH_3DES_EDE_CBC_SHA\" />"}1' \
     conf/activemq.xml.orig >> conf/activemq.xml
   ) && { cat >"$prefix"/bin/activemq-nowrapper <<-EOF
 #!/bin/sh
-test -d /tmp/activemq || mkdir /tmp/activemq
+test -d /tmp/activemq/tmp || mkdir -p /tmp/activemq/tmp
 test -d /data && ACTIVEMQ_DATA=/data || ACTIVEMQ_DATA="$prefix"/apache-activemq/data
+
 exec java -Xms1G -Xmx1G -Djava.util.logging.config.file=logging.properties -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote \
--Djava.io.tmpdir=/tmp/activemq \
+-Djava.io.tmpdir=/tmp/activemq/tmp \
 -Dactivemq.classpath="$prefix"/apache-activemq/conf \
 -Dactivemq.home="$prefix"/apache-activemq \
 -Dactivemq.base="$prefix"/apache-activemq \
