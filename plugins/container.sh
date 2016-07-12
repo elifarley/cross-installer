67c2a27ccf945d46bd5b2840c30fe7690b59700e
Elifarley C <elifarley@users.noreply.github.com>
2016-07-12 16:21:38 -0300

    tini and gosu: call "check_hash"

--- plugins/container.sh
+++ plugins/container.sh
@@ -113,29 +113,30 @@ add_user_apt() {
 }
 
 add_tini() {
-  test $# = 2 || {
-    echo "Usage: $0 install tini <version> <sha1>"
+  test $# -ge 1 || {
+    echo "Usage: $0 install tini <version>"
     return 1
   }
   local version="$1"; shift
-  local sha="$1"; shift
   local url=https://github.com/krallin/tini/releases/download/"$version"/tini-static
   curl -fsSL "$url" -o /bin/tini || {
     echo "Please check URL '$url'"
     return 1
   }
-  chmod +x /bin/tini && echo "$sha  /bin/tini" | sha1sum -wc -
+  check_hash /bin/tini "tini:$version" && \
+  chmod +x /bin/tini
 }
 
 add_gosu() {
-  test $# = 2 || {
-    echo "Usage: $0 install tini <version> <sha1>"
+  test $# -ge 1 || {
+    echo "Usage: $0 install tini <version>"
     return 1
   }
   local version="$1"; shift
   local sha="$1"; shift
   curl -fsSL https://github.com/tianon/gosu/releases/download/"$version"/gosu-amd64 -o /bin/gosu && \
-    chmod 755 /bin/gosu && echo "$sha  /bin/gosu" | sha1sum -wc -
+  check_hash /bin/gosu "gosu:$version" && \
+  chmod 755 /bin/gosu
 }
 
 cleanup() {
