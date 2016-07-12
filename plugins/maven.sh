8924c3b5fe39e3f602c4cc71284a3605b6a32fdd
Elifarley C <elifarley@users.noreply.github.com>
2016-07-12 18:09:47 -0300

    Delegate installation to "untar_url"

--- plugins/maven.sh
+++ plugins/maven.sh
@@ -1,16 +1,15 @@
 add_maven3() {
-  test $# -ge 2 || {
-    echo "Usage: $0 install maven3 <version> <sha1> [<prefix>=/usr/local]"
+  test $# -ge 1 || {
+    echo "Usage: $0 install maven3 <version> [<prefix>=/usr/local]"
     return 1
   }
 
   local version="$1"; shift
-  local sha="$1"; shift
   local prefix="${1:-/usr/local}"; test $# -gt 0 && shift
 
-  curl -fsSL http://www-us.apache.org/dist/maven/maven-3/"$version"/binaries/apache-maven-"$version"-bin.tar.gz \
-    -o /tmp/maven.tgz && echo "$sha  /tmp/maven.tgz" | sha1sum -wc - && \
-    tar -xzf /tmp/maven.tgz -C "$prefix" && rm /tmp/maven.tgz || return
-  ln -s apache-maven-"$version" "$prefix"/maven-3 && \
-  ln -s ../maven-3/bin/mvn ../maven-3/bin/mvnDebug ../maven-3/bin/mvnyjp "$prefix"/bin
+  local url_maven3='http://www-us.apache.org/dist/maven/maven-3/%s/binaries/apache-maven-%s-bin.tar.gz'
+
+  local url="$(printf "$url_maven3" "$version" "$version")"
+  untar_url --hash-id "maven:$version" --prefix "$prefix" "$url" "$version"
+
 }
