262724b5f128d6dafa907a2bf88fc7f5441b214e
Elifarley <elifarley@gmail.com>
2018-06-18 12:54:26 -0300

    github-installer.sh: Fixes

--- install.sh
+++ install.sh
@@ -1,35 +1,70 @@
-CMD_BASE="$(readlink -f $0)" || CMD_BASE="$0"; CMD_BASE="$(dirname $CMD_BASE)"
+#!/bin/sh
+CMD_BASE="$(readlink -f "$0")" || CMD_BASE="$0"; CMD_BASE="$(dirname "$CMD_BASE")"
 
-test "$DEBUG" && set -x
+dir_count_nodot() { test -d "$1" && echo $(\ls -Uq "$1" 2>/dev/null | wc -l ) ;}
 
-prefix="${1:-/usr/local}"
+githubInstall() {
+test $(( $# >= 1)) -ne 0 || {
+  echo 'Usage:'
+  echo "$0 <project> {prefix=/usr/local} {executable=bin/*}"
+  echo 'Examples:'
+  echo "$0 elifarley/cross-installer /usr/local xinstall"
+  echo "$0 elifarley/cross-installer"
+  return 1
+}
+local project="$1"; test $# -gt 0 && shift
+local projname="$(basename "$project")"
+test "$projname" || {
+  echo "<projname> missing"; return 1
+}
+test "$projname" = "$project" && {
+  echo "<project> must have a slash"; return 1
+}
 
-test -L "$prefix"/bin/xinstall && {
+prefix="${1:-/usr/local}"; test $# -gt 0 && shift
+
+executable="$1"; test $# -gt 0 && shift
+
+test -e "$prefix/$projname" -o "$executable" -a -L "$prefix/bin/$executable" && {
   test -z "$FORCE" && {
-    ls -Falk "$prefix"/bin/xinstall
+    ls -Falk "$prefix/$projname"
     echo "Previous installation exists. Aborting.
 You can set env var 'FORCE=1' to force installation."
     exit 1
   }
 
-  rm -rf "$prefix"/cross-installer "$prefix"/bin/xinstall || exit $?
+  test "$projname" && rm -rf "$prefix/$projname"
+  test "$executable" && rm -rf "$prefix/bin/$executable"
 }
 
-mkdir "$prefix"/cross-installer && tmp="$(mktemp -d)" || exit $?
+mkdir "$prefix/$projname" && tmp="$(mktemp -d)" || exit
 
-local_archive="$CMD_BASE"/cross-installer.tgz
+local_archive="$CMD_BASE/$projname".tgz
 if test -s "$local_archive" ; then
-  tar -xzf "$CMD_BASE"/cross-installer.tgz -C "$tmp" || { rm -rf "$tmp"; exit 1 ;}
+  tar -xzf "$CMD_BASE/$projname".tgz -C "$tmp" || { rm -rf "$tmp"; exit 1 ;}
 
 elif type curl >/dev/null 2>&1 ; then
-  curl -fsSL https://github.com/elifarley/cross-installer/archive/master.tar.gz \
+  curl -fsSL "https://github.com/$project"/archive/master.tar.gz \
   | tar -xz -C "$tmp" || { rm -rfv "$tmp"; exit 1 ;}
 
 else
-  echo "Unable to download cross-installer." && exit 1
+  echo "Unable to download ''$projname'." && exit 1
 fi
 
-mv "$tmp"/*/* "$prefix"/cross-installer || { rm -rfv "$tmp"; exit 1 ;}
-rm -rf "$tmp" || exit $?
+mv "$tmp"/*/* "$prefix/$projname" || { rm -rfv "$tmp"; exit 1 ;}
+rm -rf "$tmp" || exit
 
-ln -sv ../cross-installer/bin/xinstall "$prefix"/bin
+test "$executable" || { # No executable defined ?
+  # Only one executable found? TODO Create symlinks for bin/*
+  test "$(dir_count_nodot "$prefix/$projname/bin")" -eq 1 && \
+    executable="$(basename $(find "$prefix/$projname"/bin -mindepth 1 -name '.*' -prune -o -print -quit))"
+}
+
+test "$executable" && \
+  ln -sv ../"$projname/bin/$executable" "$prefix"/bin
+
+}
+
+test "$DEBUG" && set -x
+
+githubInstall elifarley/cross-installer /usr/local xinstall
